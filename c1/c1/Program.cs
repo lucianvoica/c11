3c6dcd0a4f4c2e2762f93bd787a260859610adc3
VoicaLucian09 <voica.lucian94@gmail.com>
2021-02-02 21:56:59 +0200

    curs 2

--- c1/c1/Program.cs
+++ c1/c1/Program.cs
@@ -6,57 +6,110 @@ namespace c1
     {
         static void Main(string[] args)
         {
-            //voi face un program de inceput
+            ////voi face un program de inceput
 
-            /* acesta este
-               un comentariu
-               pe mai multe linii */
-            string Mesaj = "prima valoare";
-            
-            string mesaj;
-            mesaj = "a doua valoare";
+            ///* acesta este
+            //   un comentariu
+            //   pe mai multe linii */
+            //string Mesaj = "prima valoare";
 
-            sbyte signedByte = 127;
-            byte Byte = 34;
+            //string mesaj;
+            //mesaj = "a doua valoare";
 
-            char caracter = 'd';
-            short Short = 4584;
+            //sbyte signedByte = 127;
+            //byte Byte = 34;
 
-            float a = 3.5F;
-            float b = 35;
+            //char caracter = 'd';
+            //short Short = 4584;
 
-            double c = 3.5;
-            double d = 3D;
+            //float a = 3.5F;
+            //float b = 35;
 
-            decimal e = 3.5M;
+            //double c = 3.5;
+            //double d = 3D;
 
-            //citim de la tastatura un mesaj
-            string mesajDeLaTastatura = Console.ReadLine();
+            //decimal e = 3.5M;
 
-            int numar1 = int.Parse(mesajDeLaTastatura);
+            ////citim de la tastatura un mesaj
+            //string mesajDeLaTastatura = Console.ReadLine();
 
-            Console.WriteLine(numar1 * numar1);
+            //int numar1 = int.Parse(mesajDeLaTastatura);
 
-            int x = 0;
-            int y = 1;
+            //Console.WriteLine(numar1 * numar1);
 
-            //x = y++; // x = y; y = y + 1; -> x = 1 , y = 2;
+            //int x = 0;
+            //int y = 1;
 
-            //x = ++y; // y = y + 1; x = y; -> x = 2 , y = 2; 
+            ////x = y++; // x = y; y = y + 1; -> x = 1 , y = 2;
 
-            x = 5;
-            y = 2;
-            /* x += y; // x = x + y;  --> x = 7, y = 2
-            x -= y; // x = x - y;
-            x /= y; // x = x / y;  --> x = 2, y = 2 */
-            x %= y; // x = x % y;  --> x = 1, y = 2;
-            x *= y; // x = x * y;
+            ////x = ++y; // y = y + 1; x = y; -> x = 2 , y = 2; 
 
-            string textCompus = "Text" + "Doi"; //TextDoi
+            //x = 5;
+            //y = 2;
+            ///* x += y; // x = x + y;  --> x = 7, y = 2
+            //x -= y; // x = x - y;
+            //x /= y; // x = x / y;  --> x = 2, y = 2 */
+            //x %= y; // x = x % y;  --> x = 1, y = 2;
+            //x *= y; // x = x * y;
 
-            x += y; // x = x + y;
+            //string textCompus = "Text" + "Doi"; //TextDoi
 
-            short variabila = 34;
+            //x += y; // x = x + y;
+
+            //short variabila = 34;
+
+            //int a = 10;
+
+            //bool rezultat = (a != 10);
+
+            //bool a = false;
+            //bool b = true;
+            //bool c = true;
+            //bool rezultat;
+
+            //rezultat = !b ;
+
+            //double d = 34.23;
+            //int i;
+
+            //i = (int) d;
+
+            //short a = 32650;
+            //short b = 32650;
+            ////a + b = 65300;
+
+            //string rezultat = (a + b).ToString(); // -236
+
+
+            //int a = 4;
+            //float b = 4.4F;
+            //decimal c = 4.4M;
+            //double d = 4.4;
+
+            //Console.WriteLine(b.ToString());
+
+            //int e = -23;
+
+            //Console.WriteLine(Convert.ToBoolean(e));
+            //// true daca e != 0
+            //// false daca e == 0
+
+            if (1 == 2 && false) //expresie care sa fie evaluata la bool
+            {
+                Console.WriteLine("rezultatul este adevarat");
+            }
+            else if (3 == 4)
+            {
+                Console.WriteLine("rezultatul 2 este adevarat");
+            }
+            else
+            {
+                Console.WriteLine("rezultatele 1 si 2 sunt false");
+            }
+
+
+
+            Console.WriteLine();
 
             Console.ReadKey();
         }
---
+++ c1/c2/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c2
+{
+    class Program
+    {
+        enum ZileSaptamana
+        {
+            Luni = 1,
+            Marti,
+            Miercuri,
+            Joi,
+            Vineri,
+            Sambata,
+            Duminica
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Introduceti numarul: ");
+            //string mesajDeLaTastatura = Console.ReadLine();
+            //int numar = int.Parse(mesajDeLaTastatura);
+
+            //if(numar >= 10)
+            //{
+            //    Console.WriteLine("numarul este >= 10");
+            //} 
+            //else
+            //{
+            //    Console.WriteLine("numarul este < 10");
+            //}
+            int i;
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i * i);
+            }
+            Console.WriteLine(i); // 3
+
+            for (i = 3; i < 10; i = i + 2)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            int nr = 3;
+            while (nr < 10)
+            {
+                Console.Write(nr + " ");
+
+                nr += 2;
+            }
+
+            Console.WriteLine();
+
+            nr = 3;
+            do
+            {
+                Console.Write(nr + " ");
+
+                nr += 2;
+            } while (nr < 10);
+
+            const double PI = 3.141592;
+
+
+            Console.Write("Introduceti ziua saptamanii: ");
+            string mesajDeLaTastatura = Console.ReadLine();
+            int numar = int.Parse(mesajDeLaTastatura);
+
+
+            if (numar == (int)ZileSaptamana.Luni)
+            {
+                Console.WriteLine("Laeroi");
+            }
+            else if (numar == (int)ZileSaptamana.Marti)
+            {
+                Console.WriteLine("lsdjfdsj");
+            }
+            else if (numar == (int)ZileSaptamana.Miercuri)
+            {
+                Console.WriteLine("ggfg");
+            }
+            else if (numar == (int)ZileSaptamana.Joi)
+            {
+                Console.WriteLine("gfgfgf");
+            }
+
+            switch (numar)
+            {
+                case (int)ZileSaptamana.Luni:
+                case (int)ZileSaptamana.Marti:
+                    Console.WriteLine("test");
+                    break;
+                case (int)ZileSaptamana.Miercuri:
+                    Console.WriteLine("ggfg");
+                    break;
+                case (int)ZileSaptamana.Joi:
+                    Console.WriteLine("gfgfgf");
+                    break;
+            }
+
+            for (int j = 0; j < 10; j++)
+            {
+                if (j == 5)
+                {
+                    break;
+                }
+                Console.Write(j + " ");
+            }
+            Console.WriteLine("final");  // 0 1 2 3 4 final
+
+
+            for (int j = 0; j < 10; j++)
+            {
+                if (j == 5)
+                {
+                    continue;
+                }
+
+                Console.Write(j + " ");
+            }
+            Console.WriteLine("final");  // 0 1 2 3 4 6 7 8 9 final
+
+            mesajDeLaTastatura = Console.ReadLine();
+            int n = int.Parse(mesajDeLaTastatura);
+            int z, p, c;
+            z = 0;
+            p = 1;
+            while(n > 0)
+            {
+                c = n % 10;
+                n = n / 10;
+                if(c % 3 == 0)
+                {
+                    z = z + p * (9 - c);
+                    p = p * 10;
+                }
+            }
+            Console.WriteLine(z);
+
+            Console.ReadKey();
+        }
+    }
+}
