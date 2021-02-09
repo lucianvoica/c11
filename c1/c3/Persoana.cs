f9182d67c1ce47d91bac7957857a9ed5dbef6cc6
VoicaLucian09 <voica.lucian94@gmail.com>
2021-02-09 21:34:13 +0200

    curs3

---
+++ c1/c3/Persoana.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c3
+{
+    class Persoana
+    {
+        private bool esteBarbat;
+        private string cnp;
+        public string adresa;
+
+        public string Nume { get; set; }
+        public int Varsta { get; set; }
+        public string Prenume { get; set; }
+
+        public int PinCard { get; set; }
+
+        public bool EsteBarbat
+        {
+            get
+            {
+                return this.esteBarbat;
+            }
+            set
+            {
+                this.esteBarbat = value;
+            }
+        }
+
+        public string Cnp
+        {
+            get
+            {
+                return this.cnp;
+            }
+            set
+            {
+                this.cnp = value;
+            }
+        }
+
+        //public string GetNume()
+        //{
+        //    return this.nume;
+        //}
+        //public void SetNume(string nume)
+        //{
+        //    this.nume = nume;
+        //}
+        //public string GetPrenume()
+        //{
+        //    return this.prenume;
+        //}
+        //public void SetPrenume(string noulPrenume)
+        //{
+        //    this.prenume = noulPrenume;
+        //}
+        //public int GetVarsta()
+        //{
+        //    return varsta;
+        //}
+        //public void SetVarsta(int varsta)
+        //{
+        //    this.varsta = varsta;
+        //}
+
+        //public void SetNumeSiPrenume(string nume, string prenume = "prenume default")
+        //{
+        //    this.prenume = prenume;
+        //    this.nume = nume;
+        //}
+
+        //public void PrintNume()
+        //{
+        //    Console.WriteLine(nume);
+        //}
+
+        //public void PrintNumeComplet()
+        //{
+        //    string rezultat = GetNumeComplet();
+        //    Console.WriteLine(rezultat);
+        //}
+
+        //public string GetNumeComplet()
+        //{
+        //    return prenume + " " + nume;
+        //}
+
+        //public void SchimbaPrenumele(string prenumeNou)
+        //{
+        //    prenume = prenumeNou;
+        //}
+
+        //public void SchimbaNumePrenumeCNP(
+        //    string noulNume, 
+        //    string noulPrenume = "PRENUME", 
+        //    string noulCnp = "CNP")
+        //{
+        //    nume = noulNume;
+        //    prenume = noulPrenume;
+        //    cnp = noulCnp;
+        //}
+
+    }
+}
--- c1/c3/Program.cs
+++ c1/c3/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace c3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //1
+            Student s1;
+
+            //2
+            Student s2;
+            s2 = new Student();
+
+            //3
+            s1 = new Student();
+
+            //4
+            Elev e1 = new Elev();
+
+            Persoana p3 = new Persoana();
+            p3.adresa = "Bucuresti";
+
+            //p3.SetNumeSiPrenume("Voica", "Lucian");
+            //p3.SetNumeSiPrenume(prenume: "Lucian", nume: "Voica");
+
+            Persoana p4 = new Persoana();
+
+            //p4.SchimbaNumePrenumeCNP("NUME", "PRENUME", "1000");
+
+            //p4.PrintNumeComplet();
+
+            Persoana pers1 = new Persoana();
+            //pers1.SetNume("DO_IT_ACADEMY");
+            //pers1.SetPrenume("Cursant");
+            //pers1.SetVarsta(20);
+
+            //Console.WriteLine(pers1.GetNume() + " " 
+            //    + pers1.GetPrenume() + " " 
+            //    + pers1.GetVarsta());
+
+            pers1.Nume = "DO_IT_ACADEMY";
+            pers1.Prenume = "Cursant";
+
+
+            Console.WriteLine(pers1.Nume + " " + pers1.Prenume);
+
+            //string numeCompletP4 = p4.GetNumeComplet();
+            Console.WriteLine(numeCompletP4);
+
+            Console.ReadKey();
         }
     }
 }
