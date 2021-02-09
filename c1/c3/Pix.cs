eeb9e4a0e19552d5222c66837e380438858f450e
VoicaLucian09 <voica.lucian94@gmail.com>
2021-02-09 18:22:14 +0200

    curs3 start

---
+++ c1/c3/Pix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c3
+{
+    public class Pix
+    {
+        public bool EsteDeschis { get; set; }
+        public string Culoare { get; set; }
+        public string CuloarePasta { get; set; }
+        public Pix()
+        {
+            EsteDeschis = false;
+            Culoare = "alb";
+            CuloarePasta = "albastru";
+        }
+        public void DeschidePix()
+        {
+            EsteDeschis = true;
+        }
+        public void Scrie(string mesaj)
+        {
+            if (EsteDeschis)
+            {
+                Console.WriteLine(CuloarePasta + ": " + mesaj);
+            }
+        }
+        public void InchidePix()
+        {
+            EsteDeschis = false;
+        }
+    }
+}
